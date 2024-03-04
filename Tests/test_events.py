708774455f775592ffdd8fe4fe1b6dc07546a205
Pranjal <pranjaltimsina@lftechnology.com>
2024-03-04 15:01:14 +0545

    Added requests for event's APIs

--- Tests/test_events.py
+++ Tests/test_events.py
@@ -6,18 +6,86 @@ from utilities import post_request
 from utilities import put_request
 from utilities import delete_request
 
+
 @pytest.fixture(scope="module")
 def access_token():
     token = get_access_token()
     yield token
+
 
 def test_get_events_positive(access_token):
     headers = {"Authorization": f"Bearer {access_token}"}
     response = get_request("events", headers=headers)
     assert response.status_code == 200
 
+
 def test_get_events_negetive(access_token):
     headers = {"Authorization": f"Bearer "}
     response = get_request("events", headers=headers)
     assert response.status_code == 401
 
+
+def test_post_events_positive(access_token):
+    headers = {"Authorization": f"Bearer {access_token}"}
+    payload = {
+        'name': Data.name_generator(access_token),
+        'description': Data.description_generator(access_token),
+        'link': None,
+        'eventDate': Data.date_generator(access_token),
+    }
+    response = post_request("events", headers, payload)
+    assert response.status_code == 201
+    data_set = response.json()
+    assert data_set['message'] == 'Event created successfully.'
+    data_id = data_set['data']['id']
+    fetch_data = {
+        'id': data_id,
+        'name': payload['name']
+
+    }
+    return fetch_data
+
+
+def test_post_events_negetive(access_token):
+    headers = {"Authorization": f"Bearer {access_token}"}
+    payload = {
+        'name': '',
+        'description': Data.description_generator(access_token),
+        'link': None,
+        'eventDate': Data.date_generator(access_token),
+    }
+    response = post_request("events", headers, payload)
+    assert response.status_code == 400
+
+
+def test_update_events_positive(access_token):
+    data = test_post_events_positive(access_token)
+    headers = {"Authorization": f"Bearer {access_token}"}
+    payload = {
+        'name': Data.name_generator(access_token),
+        'description': Data.description_generator(access_token),
+        'link': None,
+        'eventDate': Data.date_generator(access_token),
+    }
+    response = put_request(f"events/{data['id']}", headers, payload)
+    assert response.status_code == 200
+
+
+def test_update_events_negetive(access_token):
+    data = test_post_events_positive(access_token)
+    headers = {"Authorization": f"Bearer {access_token}"}
+    payload = {
+        'name': '',
+        'description': Data.description_generator(access_token),
+        'link': None,
+        'eventDate': Data.date_generator(access_token),
+    }
+    response = put_request(f"events/{data['id']}", headers, payload)
+    assert response.status_code == 400
+
+
+def test_delete_events(access_token):
+    data = test_post_events_positive(access_token)
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = delete_request(f"events/{data['id']}", headers)
+    assert response.status_code == 200

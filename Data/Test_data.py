5c1d8c5f137b06ccc22cd87bc2d434e18a5d1ed5
Pranjal <pranjaltimsina@lftechnology.com>
2024-03-05 17:16:25 +0545

    Added self in method

--- Data/Test_data.py
+++ Data/Test_data.py
@@ -1,17 +1,17 @@
 from faker import Faker
 
 class Data:
-    def name_generator(access_token):
+    def name_generator(self):
         fake= Faker()
         full_name= fake.name()
         return full_name
 
-    def description_generator(access_token):
+    def description_generator(self):
         fake= Faker()
         description= fake.name()
         return description
 
-    def date_generator(access_token):
+    def date_generator(self):
         fake = Faker()
         date = fake.date()
         return date

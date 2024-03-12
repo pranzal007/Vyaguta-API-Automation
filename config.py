68c8672cc7de5a5ce14bfa2010f2860e265120df
Pranzal <99467372+pranzal007@users.noreply.github.com>
2024-03-12 11:18:24 +0545

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-class configure:
-    authorization_url= "https://qa.vyaguta.lftechnology.com.np/api/auth/authorize"
-    base_url = "https://qa.vyaguta.lftechnology.com.np/api/core"
-    client_id= "lms"
-    token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoiNDAwIn0sImlhdCI6MTU4NTExNjQzMH0.b-XFiavU0CI_V06xSqnZf2o7n4MGA9ojIn3SEiyHSho"

dabb37afdc6a4f3664b8483ac71ec155d2c9a063
Pranjal <pranjaltimsina@lftechnology.com>
2024-03-22 21:14:52 +0545

    Added api_request fixture to return the APIRequest class

--- Tests/test_events.py
+++ Tests/test_events.py
@@ -1,31 +1,31 @@
 import pytest
-from utilities import get_request
 from Data.Test_data import Data
-from login_setup import get_access_token
-from utilities import post_request
-from utilities import put_request
-from utilities import delete_request
-
+from login_setup import LoginSetup
+from utilities import ApiRequest
 
 @pytest.fixture(scope="module")
 def access_token():
-    token = get_access_token()
+    login = LoginSetup()
+    token = login.get_access_token()
     yield token
 
+@pytest.fixture(scope="module")
+def api_request():
+    return ApiRequest()
 
-def test_get_events_positive(access_token):
+def test_get_events_positive(access_token,api_request):
     headers = {"Authorization": f"Bearer {access_token}"}
-    response = get_request("events", headers=headers)
+    response = api_request.get_request("events", headers=headers)
     assert response.status_code == 200
 
 
-def test_get_events_negetive(access_token):
+def test_get_events_negetive(access_token,api_request):
     headers = {"Authorization": f"Bearer "}
-    response = get_request("events", headers=headers)
+    response = api_request.get_request("events", headers=headers)
     assert response.status_code == 401
 
 
-def test_post_events_positive(access_token):
+def test_post_events_positive(access_token,api_request):
     headers = {"Authorization": f"Bearer {access_token}"}
     payload = {
         'name': Data.name_generator(self=None),
@@ -33,7 +33,7 @@ def test_post_events_positive(access_token):
         'link': None,
         'eventDate': Data.date_generator(self=None),
     }
-    response = post_request("events", headers, payload)
+    response = api_request.post_request("events", headers, payload)
     assert response.status_code == 201
     data_set = response.json()
     assert data_set['message'] == 'Event created successfully.'
@@ -46,7 +46,7 @@ def test_post_events_positive(access_token):
     return fetch_data
 
 
-def test_post_events_negetive(access_token):
+def test_post_events_negetive(access_token,api_request):
     headers = {"Authorization": f"Bearer {access_token}"}
     payload = {
         'name': '',
@@ -54,12 +54,12 @@ def test_post_events_negetive(access_token):
         'link': None,
         'eventDate': Data.date_generator(self=None),
     }
-    response = post_request("events", headers, payload)
+    response = api_request.post_request("events", headers, payload)
     assert response.status_code == 400
 
 
-def test_update_events_positive(access_token):
-    data = test_post_events_positive(access_token)
+def test_update_events_positive(access_token,api_request):
+    data = test_post_events_positive(access_token,api_request)
     headers = {"Authorization": f"Bearer {access_token}"}
     payload = {
         'name': Data.name_generator(self=None),
@@ -67,12 +67,12 @@ def test_update_events_positive(access_token):
         'link': None,
         'eventDate': Data.date_generator(self=None),
     }
-    response = put_request(f"events/{data['id']}", headers, payload)
+    response = api_request.put_request(f"events/{data['id']}", headers, payload)
     assert response.status_code == 200
 
 
-def test_update_events_negetive(access_token):
-    data = test_post_events_positive(access_token)
+def test_update_events_negetive(access_token,api_request):
+    data = test_post_events_positive(access_token,api_request)
     headers = {"Authorization": f"Bearer {access_token}"}
     payload = {
         'name': '',
@@ -80,12 +80,12 @@ def test_update_events_negetive(access_token):
         'link': None,
         'eventDate': Data.date_generator(self=None),
     }
-    response = put_request(f"events/{data['id']}", headers, payload)
+    response = api_request.put_request(f"events/{data['id']}", headers, payload)
     assert response.status_code == 400
 
 
-def test_delete_event(access_token):
-    data = test_post_events_positive(access_token)
+def test_delete_event(access_token,api_request):
+    data = test_post_events_positive(access_token,api_request)
     headers = {"Authorization": f"Bearer {access_token}"}
-    response = delete_request(f"events/{data['id']}", headers)
+    response = api_request.delete_request(f"events/{data['id']}", headers)
     assert response.status_code == 200

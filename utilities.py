5ef1f9baadab393c218cc47b0b23f52af71e1609
Pranjal <pranjaltimsina@lftechnology.com>
2024-03-22 21:13:12 +0545

    Added ApiRequest class and instance of the class

--- utilities.py
+++ utilities.py
@@ -1,21 +1,27 @@
 import requests
-from config import configure
+from dotenv import dotenv_values
 
-def get_request(endpoint, headers=None):
-    url = f"{configure.base_url}/{endpoint}"
-    response = requests.get(url, headers=headers)
-    return response
-def post_request(endpoint,headers=None,payload=None):
-    url= f"{configure.base_url}/{endpoint}"
-    response = requests.post(url,headers=headers,json=payload)
-    return response
+class ApiRequest:
+    def __init__(self):
+        self.config = dotenv_values("config.env")
+        self.base_url = self.config['base_url']
 
-def put_request(endpoint,headers=None, payload=None):
-    url= f"{configure.base_url}/{endpoint}"
-    response = requests.put(url,headers=headers,json=payload)
-    return response
+    def get_request(self, endpoint, headers=None):
+        url = f"{self.base_url}/{endpoint}"
+        response = requests.get(url, headers=headers)
+        return response
 
-def delete_request(endpoint,headers=None):
-    url= f"{configure.base_url}/{endpoint}"
-    response = requests.delete(url,headers=headers)
-    return response
+    def post_request(self, endpoint, headers=None, payload=None):
+        url = f"{self.base_url}/{endpoint}"
+        response = requests.post(url, headers=headers, json=payload)
+        return response
+
+    def put_request(self, endpoint, headers=None, payload=None):
+        url = f"{self.base_url}/{endpoint}"
+        response = requests.put(url, headers=headers, json=payload)
+        return response
+
+    def delete_request(self, endpoint, headers=None):
+        url = f"{self.base_url}/{endpoint}"
+        response = requests.delete(url, headers=headers)
+        return response
